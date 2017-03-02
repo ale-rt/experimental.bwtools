2655025d90971e44a2998e2e2735c03ca29715cc
Alessandro Pisa <alessandro.pisa@gmail.com>
2017-03-02 15:33:21 +0100

    Test registry

--- src/experimental/bwtools/tests/test_robot.py
+++ src/experimental/bwtools/tests/test_robot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from experimental.bwtools.testing import EXPERIMENTAL_BWTOOLS_ACCEPTANCE_TESTING  # noqa
 from plone.app.testing import ROBOT_TEST_LEVEL
 from plone.testing import layered
-from experimental.bwtools.testing import EXPERIMENTAL_BWTOOLS_ACCEPTANCE_TESTING  # noqa
 
 import os
 import robotsuite
--- src/experimental/bwtools/tests/test_setup.py
+++ src/experimental/bwtools/tests/test_setup.py
@@ -1,32 +1,38 @@
 # -*- coding: utf-8 -*-
-"""Setup tests for this package."""
-from plone import api
+'''Setup tests for this package.'''
 from experimental.bwtools.testing import EXPERIMENTAL_BWTOOLS_INTEGRATION_TESTING  # noqa
+from plone import api
 
 import unittest
 
 
 class TestSetup(unittest.TestCase):
-    """Test that experimental.bwtools is properly installed."""
+    '''Test that experimental.bwtools is properly installed.'''
 
     layer = EXPERIMENTAL_BWTOOLS_INTEGRATION_TESTING
 
     def setUp(self):
-        """Custom shared utility setup for tests."""
+        '''Custom shared utility setup for tests.'''
         self.portal = self.layer['portal']
         self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
-        """Test if experimental.bwtools is installed."""
+        '''Test if experimental.bwtools is installed.'''
         self.assertTrue(self.installer.isProductInstalled(
             'experimental.bwtools'))
 
     def test_browserlayer(self):
-        """Test that IExperimentalBwtoolsLayer is registered."""
+        '''Test that IExperimentalBwtoolsLayer is registered.'''
         from experimental.bwtools.interfaces import (
             IExperimentalBwtoolsLayer)
         from plone.browserlayer import utils
         self.assertIn(IExperimentalBwtoolsLayer, utils.registered_layers())
+
+    def test_registry(self):
+        '''Test that the registry records are correctly installed.
+        '''
+        pr = api.portal.get_tool('portal_registry')
+        self.assertIn('experimental.bwtools.known_bad_ips', pr)
 
 
 class TestUninstall(unittest.TestCase):
@@ -39,13 +45,23 @@ class TestUninstall(unittest.TestCase):
         self.installer.uninstallProducts(['experimental.bwtools'])
 
     def test_product_uninstalled(self):
-        """Test if experimental.bwtools is cleanly uninstalled."""
+        '''Test if experimental.bwtools is cleanly uninstalled.'''
         self.assertFalse(self.installer.isProductInstalled(
             'experimental.bwtools'))
 
     def test_browserlayer_removed(self):
-        """Test that IExperimentalBwtoolsLayer is removed."""
+        '''Test that IExperimentalBwtoolsLayer is removed.'''
         from experimental.bwtools.interfaces import \
             IExperimentalBwtoolsLayer
         from plone.browserlayer import utils
         self.assertNotIn(IExperimentalBwtoolsLayer, utils.registered_layers())
+
+    def test_registry_cleaned(self):
+        '''Test that the registry records are correctly installed.
+        '''
+        pr = api.portal.get_tool('portal_registry')
+        package_keys = [
+            key for key in pr.keys()
+            if key.startswith('experimental.bwtools')
+        ]
+        self.assertListEqual(package_keys, [])
